b3603f9638be38424ecf7dec0d4d4a9296ea8dff
leo004288 <leo004288@gmail.com>
2026-06-24 12:36:52 +0900

    admin update

--- src/main/java/com/example/springedu2/controller/AdminController.java
+++ src/main/java/com/example/springedu2/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.springedu2.controller;
 
 import com.example.springedu2.dto.MemberCreateForm;
+import com.example.springedu2.dto.MemberUpdateForm;
 import com.example.springedu2.entity.Member;
 import com.example.springedu2.service.memberService;
 import jakarta.validation.Valid;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -33,7 +35,7 @@ public class AdminController {
         return "memberList";  // memberList.html
     }
 
-    // 회원추가(관리자)
+    // 회원추가
     @PostMapping("/admin/members")
     public String adminCreate(
             @Valid @ModelAttribute("memberForm") MemberCreateForm memberCreateForm,
@@ -43,7 +45,7 @@ public class AdminController {
             return "/memberAdminForm"; //  다시 입력받기
         }
 
-        // 새 회원을 추가 (관리자)
+        // 새 회원을 추가
         try {
             memberService.create(memberCreateForm);
         } catch (IllegalArgumentException e) {
@@ -59,6 +61,49 @@ public class AdminController {
     public String adminCreateForm(Model model) {
         model.addAttribute("memberForm", new MemberCreateForm());
         return "memberAdminForm";
+    }
+
+    // 회원수정 화면 /admin/members/1/edit
+    @GetMapping("/admin/members/{id}/edit")
+    public String adminEditForm(@PathVariable Long id, Model model) {
+
+        // 엔티티 안의 DB 데이터조회
+        Member           member     = memberService.findById(id);
+
+        // DTO 안의 memberAdminEditForm 에서 사용할 객체인 MemberUpdateForm 구조로 변겅
+        MemberUpdateForm memberForm = memberService.toUpdateFrom(member);
+
+        model.addAttribute("memberForm", memberForm);
+        model.addAttribute("member", member);
+
+        return "memberAdminEditForm";
+    }
+
+    // 넘어온 수정정보로 수정 진행
+    @PostMapping("/admin/members/{id}/edit")
+    public String adminEdit(
+            @PathVariable Long id,
+            @Valid @ModelAttribute("memberForm") MemberUpdateForm form,
+            BindingResult bindingResult,
+            Model model) {
+
+        // 수정
+        Member member = memberService.findById(id);
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("memberForm", member);
+            return "memberAdminEditForm";
+        }
+
+        try {
+            memberService.update(id, form, true);
+        } catch (IllegalArgumentException e) {
+            bindingResult.reject("updateFail", e.getMessage());
+            model.addAttribute("member", member);
+            return "memberAdminEditForm";
+        }
+
+        return "redirect:/admin/members";
     }
 
 }
